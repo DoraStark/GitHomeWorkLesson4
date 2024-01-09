b5cab51d30e584500fa263d8960054c2f5ba96b0
DoraStark <doraschneesturmin@gmail.com>
2024-01-09 20:26:58 +0100

    New system out print added

--- src/HomeWork04.java
+++ src/HomeWork04.java
@@ -3,5 +3,6 @@ public class HomeWork04 {
     public static void main(String[] args) {
 
         System.out.println("Hello from Dora to Git");
+        System.out.println("Here is the second commit");
     }
 }
